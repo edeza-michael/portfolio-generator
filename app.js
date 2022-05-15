226fe78ed834a3da951a201741ef3fe53f0610dd
Michael Edeza-Salvant <michaeledeza-salvant@Michaels-MacBook-Pro.local>
2022-05-15 07:12:33 -0700

    generate HTML

--- app.js
+++ app.js
@@ -1,42 +1,17 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-    console.log(profileDataArgs);
+const fs = require('fs');
 
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
-// const printProfileData = (profileDataArr) => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
+const generatePage = require('./src/page-template.js');
 
-//     console.log('================');
 
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
 
-// printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-const generatePage = (userName, githubName) =>  {
-    return `
-    <!DOCTYPE html> 
-    <html lang="en"> 
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
-    </head>
-  
-    <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
-    </body>
-    </html>
-    `;
-};
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
-console.log(name,github);
-console.log(generatePage(name, github));
-
-// stopped at 9.2.5 (continue from there)
+module.exports = generatePage;
