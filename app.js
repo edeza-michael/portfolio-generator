8f9547c41ac1e1c0c8cbd0ec60eec1d8c89bec1b
3 Leches <edezamichael@gmail.com>
2022-05-16 17:44:27 -0700

    inquirer

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
+console.log(inquirer)
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-const generatePage = require('./src/page-template.js');
+// const generatePage = require('./src/page-template.js');
 
 
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+// fs.writeFile('./index.html', generatePage(name, github), err => {
+//   if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-module.exports = generatePage;
+// module.exports = generatePage;
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
